4462413f10a158a6d944e9c023d5b7fe54f7adfa
mmd <22621120+mm-ghost@users.noreply.github.com>
2023-05-30 16:12:30 +0330

    Add Minter contract

--- contracts/PION.sol
+++ contracts/PION.sol
@@ -7,7 +7,13 @@ import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract PION is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, AccessControlUpgradeable {
+contract PION is
+    Initializable,
+    ERC20Upgradeable,
+    ERC20BurnableUpgradeable,
+    PausableUpgradeable,
+    AccessControlUpgradeable
+{
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
@@ -16,7 +22,7 @@ contract PION is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, Paus
         _disableInitializers();
     }
 
-    function initialize() initializer public {
+    function initialize() public initializer {
         __ERC20_init("PION", "PION");
         __ERC20Burnable_init();
         __Pausable_init();
@@ -39,11 +45,11 @@ contract PION is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, Paus
         _mint(to, amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
         super._beforeTokenTransfer(from, to, amount);
     }
 }
