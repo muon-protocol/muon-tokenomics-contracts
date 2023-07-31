f0a9c6a36ae27be3dd2af854c90aff8bf9ea1361
Shayan Shiravani <shayan5420@yahoo.com>
2023-07-31 15:13:22 +0330

    BondedPION: change functions visibility to external

--- contracts/BondedPION.sol
+++ contracts/BondedPION.sol
@@ -4,7 +4,10 @@ pragma solidity 0.8.19;
 import "./BondedToken.sol";
 
 contract BondedPION is BondedToken {
-    function initialize(address _token, address _treasury) public initializer {
+    function initialize(
+        address _token,
+        address _treasury
+    ) external initializer {
         BondedToken._initialize(
             _token,
             _treasury,
