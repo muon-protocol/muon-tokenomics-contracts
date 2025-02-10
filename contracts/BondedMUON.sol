3fa09efd2d2826100dfe7418fb37354127e8d968
Shayan <shayan6959@gmail.com>
2025-02-10 23:28:10 +0330

    bonMuon redeem

--- contracts/BondedMUON.sol
+++ contracts/BondedMUON.sol
@@ -2,9 +2,15 @@
 pragma solidity 0.8.19;
 
 import "./BondedToken.sol";
+import "./interfaces/IEscrow.sol";
 
 contract BondedMUON is BondedToken {
+    bytes32 public constant REDEEM_ROLE = keccak256("REDEEM_ROLE");
+
     address public escrow;
+    bool public isPublicRedeemEnabled;
+
+    event Redeem(uint256 tokenId, address to, uint256 amount, address redeemer);
 
     function initialize(
         address _token,
@@ -20,4 +26,25 @@ contract BondedMUON is BondedToken {
         escrow = _escrow;
     }
 
+    function redeemBaseToken(uint256 _tokenId, uint256 _amount) external {
+        require(_amount > 0, "Invalid amount");
+        require(
+            lockedOf[_tokenId][baseToken] >= _amount,
+            "Insufficient balance"
+        );
+
+        if (!isPublicRedeemEnabled) {
+            require(hasRole(REDEEM_ROLE, msg.sender), "Redeem is limited");
+        } else {
+            require(
+                ownerOf(_tokenId) == msg.sender ||
+                    hasRole(REDEEM_ROLE, msg.sender),
+                "Permission denied"
+            );
+        }
+
+        IEscrow(escrow).redeemTo(ownerOf(_tokenId), _amount);
+
+        emit Redeem(_tokenId, ownerOf(_tokenId), _amount, msg.sender);
+    }
 }
