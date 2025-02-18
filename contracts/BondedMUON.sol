849983e1c4c913148a48a5541bff54766ba17d40
Shayan Shiravani <shayan6959@gmail.com>
2025-02-18 15:01:44 +0330

    bonMUON

--- contracts/BondedMUON.sol
+++ contracts/BondedMUON.sol
@@ -7,6 +7,8 @@ contract BondedMUON is BondedToken {
     function initialize(
         address _token,
         address _treasury,
+        uint256 _tokenIdCounter,
+        uint256 _totalLocked,
         address _escrow
     ) external initializer {
         BondedToken._initialize(
@@ -16,5 +18,32 @@ contract BondedMUON is BondedToken {
             "bonMUON",
             _escrow
         );
+        tokenIdCounter = _tokenIdCounter;
+        totalLocked[_token] = _totalLocked;
+    }
+
+    function migrate(
+        uint256[] calldata _tokenId,
+        address[] calldata _owner,
+        uint256[] calldata _balance,
+        uint256[] calldata _mintedAt
+    ) external onlyOwner {
+        uint256 length = _tokenId.length;
+        address[] memory tokens = new address[](1);
+        uint256[] memory amounts = new uint256[](1);
+        tokens[0] = baseToken;
+
+        for(uint256 i = 0; i < length; i++) {
+            uint256 nftId = _tokenId[i];
+            
+            _safeMint(_owner[i], nftId);
+
+            lockedOf[nftId][baseToken] = _balance[i];
+            mintedAt[nftId] = _mintedAt[i];
+
+            amounts[0] = _balance[i];
+
+            emit Locked(msg.sender, nftId, tokens, amounts);
+        }
     }
 }
