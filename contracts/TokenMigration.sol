58475d8fbfbb4f3c44ef93260ebc77d40cea2820
Shayan Shiravani <shayan6959@gmail.com>
2025-03-13 13:19:08 +0330

    TokenMigration: get migrations

--- contracts/TokenMigration.sol
+++ contracts/TokenMigration.sol
@@ -12,7 +12,10 @@ import "./interfaces/IToken.sol";
 contract TokenMigration is Ownable, Pausable {
     /// @notice token to be migrated/burnt
     address public baseToken;
+    uint256 public lastUserId;
 
+    /// @notice id => address
+    mapping(uint256 => address) public users;
     /// @notice user => amount
     mapping(address => uint256) public migrations;
 
@@ -38,8 +41,32 @@ contract TokenMigration is Ownable, Pausable {
         require(_amount > 0, "Invalid amount!");
 
         IToken(baseToken).burnFrom(msg.sender, _amount);
+
+        if (migrations[msg.sender] == 0) {
+            users[++lastUserId] = msg.sender;
+        }
         migrations[msg.sender] += _amount;
 
         emit Migrated(msg.sender, _amount);
+    }
+
+    function getMigrations(
+        uint256 _fromId,
+        uint256 _toId
+    )
+        external
+        view
+        returns (address[] memory _users, uint256[] memory _balances)
+    {
+        _users = new address[](_toId - _fromId + 1);
+        _balances = new uint256[](_toId - _fromId + 1);
+
+        uint256 j = 0;
+        for (uint256 i = _fromId; i <= _toId; i++) {
+            address user = users[i];
+            _users[j] = user;
+            _balances[j] = migrations[user];
+            j++;
+        }
     }
 }
