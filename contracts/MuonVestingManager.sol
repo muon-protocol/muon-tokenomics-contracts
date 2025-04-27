7a433c54195857b34c2e368a5b05a88472c09132
Shayan Shiravani <shayan6959@gmail.com>
2025-04-27 15:00:10 +0330

    vesting: release for

--- contracts/MuonVestingManager.sol
+++ contracts/MuonVestingManager.sol
@@ -3,15 +3,18 @@ pragma solidity ^0.8.0;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MuonVestingManager is Ownable {
+contract MuonVestingManager is AccessControl {
     using SafeERC20 for IERC20;
 
     struct User {
         uint256 totalAllocation;
         uint256 released;
     }
+
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant RELEASE_FOR_ROLE = keccak256("RELEASE_FOR_ROLE");
 
     address baseToken;
 
@@ -29,7 +32,9 @@ contract MuonVestingManager is Ownable {
         address _baseToken,
         uint256 _startTimestamp,
         uint256 _durationSeconds
-    ) Ownable() {
+    ) {
+        _setupRole(ADMIN_ROLE, msg.sender);
+
         baseToken = _baseToken;
         start = _startTimestamp;
         duration = _durationSeconds;
@@ -38,16 +43,13 @@ contract MuonVestingManager is Ownable {
     function bulkImport(
         address[] memory _addrs,
         uint256[] memory _balances
-    ) external onlyOwner {
+    ) external onlyRole(ADMIN_ROLE) {
         require(_addrs.length == _balances.length, "Length mismatch");
-        
+
         uint256 len = _addrs.length;
         for (uint256 i = 0; i < len; i++) {
             address addr = _addrs[i];
-            users[addr] = User(
-                _balances[i],
-                0
-            );
+            users[addr] = User(_balances[i], 0);
         }
     }
 
@@ -69,6 +71,42 @@ contract MuonVestingManager is Ownable {
         IERC20(baseToken).safeTransfer(msg.sender, amount);
 
         emit TokenReleased(msg.sender, amount);
+    }
+
+    /**
+     * @dev Release the tokens on a user's behalf.
+     *
+     * Emits a {TokenReleased} event.
+     */
+    function releaseFor(
+        address user,
+        uint256 amount
+    ) external onlyRole(RELEASE_FOR_ROLE) {
+        uint256 maxReleasableAmount = releasable(user);
+
+        require(
+            amount <= maxReleasableAmount,
+            "amount exceeds releasable amount!"
+        );
+
+        users[user].released += amount;
+
+        IERC20(baseToken).safeTransfer(msg.sender, amount);
+
+        emit TokenReleased(user, amount);
+    }
+
+    function adminWithdraw(
+        uint256 amount,
+        address _to,
+        address _tokenAddr
+    ) external onlyRole(ADMIN_ROLE) {
+        require(_to != address(0));
+        if (_tokenAddr == address(0)) {
+            payable(_to).transfer(amount);
+        } else {
+            IERC20(_tokenAddr).transfer(_to, amount);
+        }
     }
 
     /**
